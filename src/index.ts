4ac9c76c9ee9d9e8ffe86e1b16d4a2d02ef63814
williamtesche <85793944+FelipeSimoesDaRocha@users.noreply.github.com>
2021-08-31 14:06:36 -0300

    Update Npm
    "@types/dotenv": "^8.2.0",
        "@types/express": "^4.17.13",
        "@types/sqlite3": "^3.1.7",
        "dotenv": "^10.0.0",
        "express": "^4.17.1",
        "package-lock.json": "^1.0.0",
        "sqlite3": "^5.0.2",
        "ts-node": "^10.2.1",
        "ts-node-dev": "^1.1.8",
        "typescript": "^4.4.2"

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import express { Request ,Response} from 'express';
+import express { Request , Response} from 'express';
 
 const PORT = process.env.PORT || 8091;
 
 const app = express();
-app.use(bodyParser.json());
-useRoutes(app);
 
-
+app.get('/',(req: Request, res: Response) =>{
+    res.json({
+        msg: 'ok' 
+    })
+})
 app.listen(PORT, () => console.log('Servidor iniciado na porta ' + PORT));
--- src/models/products.ts
+++ src/models/products.ts
@@ -3,3 +3,4 @@ export type Product = {
     name: string;
     price: number;
 }
+ 
