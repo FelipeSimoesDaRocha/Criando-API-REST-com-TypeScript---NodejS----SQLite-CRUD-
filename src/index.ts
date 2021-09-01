e191721af6cbfcaddf75e8813e4c084b179091fc
williamtesche <85793944+FelipeSimoesDaRocha@users.noreply.github.com>
2021-09-01 18:04:42 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import dotenv from 'dotenv';
-dotenv.config();
-
-
-import express { Request , Response} from 'express';
+import express, { Request , Response} from 'express';
 import { useRoutes } from './routes';
 import bodyParser from 'body-parser';
 
